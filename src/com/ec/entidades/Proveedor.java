46494b4989e3a5ad98394d1de60f6f381aab7517
David Tayupanta <davidotb91@hotmail.com>
2015-12-06 22:16:45 -0500

    modelo inicial
    con la conexion las identidades y un packete extra con un formulario
    funcional

---
+++ src/com/ec/entidades/Proveedor.java
@@ -0,0 +1,141 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ec.entidades;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author david
+ */
+@Entity
+@Table(name = "proveedor")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Proveedor.findAll", query = "SELECT p FROM Proveedor p"),
+    @NamedQuery(name = "Proveedor.findByIdProveedor", query = "SELECT p FROM Proveedor p WHERE p.idProveedor = :idProveedor"),
+    @NamedQuery(name = "Proveedor.findByNombreProveedor", query = "SELECT p FROM Proveedor p WHERE p.nombreProveedor = :nombreProveedor"),
+    @NamedQuery(name = "Proveedor.findByRol", query = "SELECT p FROM Proveedor p WHERE p.rol = :rol")})
+public class Proveedor implements Serializable {
+
+    @Transient
+    private PropertyChangeSupport changeSupport = new PropertyChangeSupport(this);
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "ID_PROVEEDOR")
+    private Integer idProveedor;
+    @Basic(optional = false)
+    @Column(name = "NOMBRE_PROVEEDOR")
+    private String nombreProveedor;
+    @Basic(optional = false)
+    @Column(name = "ROL")
+    private String rol;
+    @JoinColumn(name = "ID_FACTU", referencedColumnName = "ID_FACTU")
+    @ManyToOne
+    private Factura idFactu;
+
+    public Proveedor() {
+    }
+
+    public Proveedor(Integer idProveedor) {
+        this.idProveedor = idProveedor;
+    }
+
+    public Proveedor(Integer idProveedor, String nombreProveedor, String rol) {
+        this.idProveedor = idProveedor;
+        this.nombreProveedor = nombreProveedor;
+        this.rol = rol;
+    }
+
+    public Integer getIdProveedor() {
+        return idProveedor;
+    }
+
+    public void setIdProveedor(Integer idProveedor) {
+        Integer oldIdProveedor = this.idProveedor;
+        this.idProveedor = idProveedor;
+        changeSupport.firePropertyChange("idProveedor", oldIdProveedor, idProveedor);
+    }
+
+    public String getNombreProveedor() {
+        return nombreProveedor;
+    }
+
+    public void setNombreProveedor(String nombreProveedor) {
+        String oldNombreProveedor = this.nombreProveedor;
+        this.nombreProveedor = nombreProveedor;
+        changeSupport.firePropertyChange("nombreProveedor", oldNombreProveedor, nombreProveedor);
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        String oldRol = this.rol;
+        this.rol = rol;
+        changeSupport.firePropertyChange("rol", oldRol, rol);
+    }
+
+    public Factura getIdFactu() {
+        return idFactu;
+    }
+
+    public void setIdFactu(Factura idFactu) {
+        Factura oldIdFactu = this.idFactu;
+        this.idFactu = idFactu;
+        changeSupport.firePropertyChange("idFactu", oldIdFactu, idFactu);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idProveedor != null ? idProveedor.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Proveedor)) {
+            return false;
+        }
+        Proveedor other = (Proveedor) object;
+        if ((this.idProveedor == null && other.idProveedor != null) || (this.idProveedor != null && !this.idProveedor.equals(other.idProveedor))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.ec.entidades.Proveedor[ idProveedor=" + idProveedor + " ]";
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        changeSupport.addPropertyChangeListener(listener);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        changeSupport.removePropertyChangeListener(listener);
+    }
+    
+}
